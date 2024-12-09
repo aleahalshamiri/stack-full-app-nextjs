0974277b894d29d034d5ffcd2427d978a0fa3c9b
Aleah Abdulkaher Alshmairi <48552043+aleahalshamiri@users.noreply.github.com>
2024-12-09 21:45:49 +0300

    install Auth.js & create OAuth on Githup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar";
+
+
+
+interface layoutProps {
+    children: React.ReactNode
+}
+
+const Layout = ({ children }: Readonly<layoutProps>) => {
+    return (
+        <main className='font-work-snas '>
+            <Navbar />
+            {children}
+        </main>
+    );
+}
+
+export default Layout;
